3d9094d963d95aa582c15792d4738a593aedc4c6
NintenDuck <andrecked507@gmail.com>
2022-04-12 20:17:13 -0500

    Added different frictions when: on_air & on_floor

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,8 @@ onready var sprite = $Sprite
 onready var animation = $AnimationPlayer
 
 export var friction = 0.0
+export var floor_friction = 0.0
+export var air_friction = 0.0
 export var vel_max = 0
 export var acceleration = 0
 export var jumpforce = 0
@@ -22,23 +24,34 @@ func _ready():
 
 
 func _physics_process(delta):
+	check_direction()
 	movement()
 	update_animations()
 	check_jump()
+	
 
-
-func movement():
+func check_direction() -> void:
 	direction = int( Input.is_action_pressed( "k_right" ) ) - int( Input.is_action_pressed( "k_left" ) )
-	
-	motion.x += direction * acceleration
 
+
+func movement() -> void:
+	# Check user input to get current direction
+
+	# 0 = neutral
+	# 1 = right
+	# 3 = left
 	if direction == 0:
-		if is_on_floor():
-			motion.x = lerp( motion.x, 0, friction )
-	
+		if not is_on_floor():
+			friction = air_friction
+		else:
+			friction = floor_friction
+			
+		motion.x = lerp( motion.x, 0, friction )
+
+	motion.x += direction * acceleration
 	motion.x = clamp( motion.x, -vel_max, vel_max )
 	motion.y += GRAVITY
-	
+
 	motion = move_and_slide(motion,UP)
 
 
@@ -48,17 +61,17 @@ func _input(event):
 			motion.y *= cut_height
 
 
-func check_jump():
+func check_jump() -> void:
 	if is_on_floor():
 		if Input.is_action_just_pressed("k_jump_action"):
 			jump(jumpforce)
 
 
-func jump(force):
+func jump(force:float) -> void:
 	motion.y -= force
 
 
-func update_animations():
+func update_animations() -> void:
 	if direction != 0:
 		sprite.scale.x = direction
 		if is_on_floor():
